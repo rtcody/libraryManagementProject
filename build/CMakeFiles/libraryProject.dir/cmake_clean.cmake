5f1ccdc6e3dbd4493da29af34e36c2204071eee2
rtcody <rylan.cody@wsu.edu>
2024-11-11 13:32:54 -0800

    build file added

---
+++ build/CMakeFiles/libraryProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libraryProject.dir/src/Book.cpp.o"
+  "CMakeFiles/libraryProject.dir/src/Book.cpp.o.d"
+  "CMakeFiles/libraryProject.dir/src/User.cpp.o"
+  "CMakeFiles/libraryProject.dir/src/User.cpp.o.d"
+  "CMakeFiles/libraryProject.dir/src/main.cpp.o"
+  "CMakeFiles/libraryProject.dir/src/main.cpp.o.d"
+  "libraryProject"
+  "libraryProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libraryProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/libraryProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libraryProject.
+# This may be replaced when dependencies are built.
